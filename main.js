b13722620ab99c3fc86319700bda0532f8537329
Abdussamaduz <kamoliddin135@gmail.com>
2021-01-31 14:18:16 +0500

    Update index.html, style.css, main.js

--- main.js
+++ main.js
@@ -8,32 +8,38 @@ let minute = date.getMinutes();
 let dateNow = `${hour}:${minute}`;
 
 let phoneTimeElement = document.querySelector('.phone-time')
+let phoneTimeEl = document.querySelector(".phone-time-two")
 
+phoneTimeEl.textContent = dateNow;
 phoneTimeElement.textContent = dateNow;
 
 
 
-let formEl = document.querySelector(".form");
-let textareaEl = document.querySelector(".texarea");
-let listEl = document.querySelector(".list")
-
-formEl.onsubmit = event => {
-    event.preventDefault();
-    let newliEl = document.createElement("li")
-    newliEl.classList.add("item", "read")
-
-
-    newliEl.textContent = textareaEl.value;
-    
-    listEl.appendChild(newliEl);
-
-    formEl.reset()
-}
-
 let formElement = document.querySelector(".form-two");
 let textareaElement = document.querySelector(".texarea-two");
+let listEl = document.querySelector(".list")
 
 formElement.onsubmit = event => {
+    event.preventDefault();
+    let newliEl = document.createElement("li")
+    newliEl.classList.add("item", "send")
+
+
+    newliEl.textContent = textareaElement.value;
+    
+    listEl.appendChild(newliEl);
+    listEl.appendChild(newliElement);
+    listEl.appendChild(newliEl);
+    listElement.appendChild(newliElement);
+
+    formElement.reset()
+}
+
+let formEl = document.querySelector(".form");
+let textareaEl = document.querySelector(".texarea");
+let listElement = document.querySelector(".list-two")
+
+formEl.onsubmit = event => {
     event.preventDefault();
 
     let newliElement = document.createElement("li");
@@ -41,8 +47,8 @@ formElement.onsubmit = event => {
 
     newliElement.textContent = textareaElement.value;
 
-    listEl.appendChild(newliElement);
+    listElement.appendChild(newliElement);
 
-    formElement.reset()
+    formEl.reset()
 
 }
