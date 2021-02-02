0537b7a52517c8a1f7604766f925207f88e77d36
Abdussamaduz <kamoliddin135@gmail.com>
2021-02-02 19:09:26 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let date = new Date();
 
 let hour = date.getHours();
@@ -14,6 +13,9 @@ phoneTimeEl.textContent = dateNow;
 phoneTimeElement.textContent = dateNow;
 
 
+let formEl = document.querySelector(".form");
+let textareaEl = document.querySelector(".texarea");
+let listElement = document.querySelector(".list-two")
 
 let formElement = document.querySelector(".form-two");
 let textareaElement = document.querySelector(".texarea-two");
@@ -21,34 +23,56 @@ let listEl = document.querySelector(".list")
 
 formElement.onsubmit = event => {
     event.preventDefault();
-    let newliEl = document.createElement("li")
-    newliEl.classList.add("item", "send")
 
+    let newliEl = document.createElement("li");
+    newliEl.classList.add("item", "send");
+    
+    
+    let newliElement = document.createElement("li");
+    newliElement.classList.add("item", "read");
+
+    newliElement.textContent = textareaElement.value;
+    // newliElement.textContent = textareaEl.value;
 
     newliEl.textContent = textareaElement.value;
+    // newliEl.textContent = textareaEl.value;
+    
+    
     
     listEl.appendChild(newliEl);
-    listEl.appendChild(newliElement);
-    listEl.appendChild(newliEl);
+    // listElement.appendChild(newliEl);
+    
+    // listEl.appendChild(newliElement);
     listElement.appendChild(newliElement);
-
-    formElement.reset()
+    
+    // formEl.reset();
+    formElement.reset();
 }
 
-let formEl = document.querySelector(".form");
-let textareaEl = document.querySelector(".texarea");
-let listElement = document.querySelector(".list-two")
 
 formEl.onsubmit = event => {
     event.preventDefault();
 
+    
+    let newliEl = document.createElement("li");
+    newliEl.classList.add("item", "send");
+
     let newliElement = document.createElement("li");
-    newliElement.classList.add("item", "send");
+    newliElement.classList.add("item", "read");
 
     newliElement.textContent = textareaElement.value;
+    newliElement.textContent = textareaEl.value;
+
+    newliEl.textContent = textareaElement.value;
+    newliEl.textContent = textareaEl.value;
 
     listElement.appendChild(newliElement);
+    listEl.appendChild(newliElement);
 
-    formEl.reset()
+    listEl.appendChild(newliEl);
+    listElement.appendChild(newliEl);
 
-}
+    formElement.reset();
+    formEl.reset();
+
+};
